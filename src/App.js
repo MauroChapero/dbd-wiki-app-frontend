45a0d59e8f1ec8d56cd09ba12260bccbdd49e0fa
MauroChapero <chaperomauro1803@gmail.com>
2023-02-09 18:56:46 -0300

    CRA project basics initialization with linter Standard presettings

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App () {
+  return (
+    <div className="App">
+      <h1 className='text-center mt-3'>Dead by Daylight Wiki Application</h1>
+    </div>
+  )
+}
+
+export default App
